8de9fb2dd9da3c7076885bf2c369f3be90076861
Leon <2381033@unai.edu>
2024-09-09 22:47:46 +0700

    Handaond2

--- latihan/Soal2.java
+++ latihan/Soal2.java
@@ -1,9 +1,25 @@
 package latihan;
 
+import java.util.Scanner;
+
 public class Soal2 {
     public static void main(String[] args) {
-        //Buatlah ssebuah program yang mencari nilai maksimum dari 3 bilangan
+        //Buatlah ssebuah program yang mencari nilai maksimum dari 3 bilanga
+        int[] bilangan = new int[3];
+        Scanner input = new Scanner(System.in);
 
+        for (int i = 0; i < bilangan.length; i++) {
+            System.out.print("Masukan Bilangan ke - " + i + " : ");
+            bilangan[i] = input.nextInt();
+        }
+
+        int largestNum = bilangan[0];
+        for (int i = 1; i < bilangan.length; i++) {
+            if (largestNum < bilangan[1]) {
+                largestNum = bilangan[i];
+            }
+        }
+        System.out.println(largestNum);
 
     }
 
